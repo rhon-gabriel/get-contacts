17c5359abb2b37067c5921226066a9023a4677a5
Rhoneil Gabriel <rhoneilgabriel@Rhoneils-Air.lan>
2022-01-31 15:03:57 +0100

    add dial code to form and validation

--- src/containers/Main/Contact/ContactForm.jsx
+++ src/containers/Main/Contact/ContactForm.jsx
@@ -1,19 +1,11 @@
-import React from "react";
-import { Form, Input, InputNumber, Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button } from "antd";
 import styles from "./Contact.module.scss";
+import axios from "axios";
 
 const ContactForm = () => {
-  const validateMessages = {
-    required: "${label} is required!",
-    types: {
-      email: "${label} is not a valid email!",
-      number: "${label} is not a valid number!",
-    },
-    number: {
-      range: "${label} must be between ${min} and ${max}",
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
+  const [dialCode, setDialCode] = useState(null);
+
   const layout = {
     labelCol: {
       span: 8,
@@ -26,6 +18,21 @@ const ContactForm = () => {
     console.log(values);
   };
 
+  const getGeoInfo = () => {
+    axios
+      .get("https://ipapi.co/json/")
+      .then((response) => {
+        let data = response.data;
+        setDialCode(data.country_calling_code);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getGeoInfo();
+  }, []);
+
   return (
     <div className={styles.contactFormContainer}>
       <h3>Find inbound call centers for your company</h3>
@@ -34,10 +41,20 @@ const ContactForm = () => {
         {...layout}
         name="nest-messages"
         onFinish={onFinish}
-        validateMessages={validateMessages}
       >
         <Form.Item
-          name={["user", "name"]}
+          name="Company"
+          label="Company"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="Company"/>
+        </Form.Item>
+        <Form.Item
+          name="Name"
           label="Name"
           rules={[
             {
@@ -45,44 +62,40 @@ const ContactForm = () => {
             },
           ]}
         >
-          <Input />
+          <Input placeholder="Full Name"/>
         </Form.Item>
         <Form.Item
-          name={["user", "email"]}
+          name="Phone"
+          label="Phone"
+          rules={[
+            {
+              required: true,
+              message: "Please input your phone number!",
+            },
+          ]}
+        >
+          <Input
+            addonBefore={dialCode}
+            style={{
+              width: "100%",
+            }}
+          />
+        </Form.Item>
+        <Form.Item
+          name="Email"
           label="Email"
           rules={[
             {
+              required: true,
               type: "email",
             },
           ]}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={["user", "email"]}
-          label="Email"
-          rules={[
-            {
-              type: "email",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={["user", "email"]}
-          label="Email"
-          rules={[
-            {
-              type: "email",
-            },
-          ]}
-        >
-          <Input />
+          <Input placeholder="name@email.com"/>
         </Form.Item>
         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
           <Button type="primary" htmlType="submit">
-            Submit
+            Get Informed
           </Button>
         </Form.Item>
       </Form>
